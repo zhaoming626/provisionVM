009dbda84447bd49a7e18d460ab7378f718bb356
Ming Zhao <zhaoming626@yeah.net>
2018-07-02 23:35:30 +0800

    update

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 # install using software
 sudo apt-get update && upgrade
-sudo apt-get install -y aptitude git zsh amule-daemon amule-utils python-pip transmission crudini apache2 jq
+sudo apt-get install -y aptitude git zsh amule-daemon amule-utils python-pip transmission crudini apache2 jq systemd
 sudo pip install shadowsocks
 
 # append public key to authorized_keys
@@ -14,34 +14,34 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 
 # set up amule-daemon and amulecmd
 sudo useradd amule
-crudini --set /etc/default/amule-damon AMULED_USER amule
-crudini --set /etc/default/amule-daemon AMULED_HOME /home/amule
+sudo crudini --set /etc/default/amule-damon AMULED_USER amule
+sudo crudini --set /etc/default/amule-daemon AMULED_HOME /home/amule
 sudo service amule-daemon restart
 
-crudini --set /home/amule/.aMule/amule.conf ExternalConnect AcceptExternalConnections 1
-crudini --set /home/amule/.aMule/amule.conf ECPassword "$amulepassword"
-amulecmd --create-config-from /home/amule/.aMule/amule.conf
+sudo crudini --set /home/amule/.aMule/amule.conf ExternalConnect AcceptExternalConnections 1
+sudo crudini --set /home/amule/.aMule/amule.conf ECPassword "$amulepassword"
+sudo amulecmd --create-config-from /home/amule/.aMule/amule.conf
 
 # set up symbolic link to serve files
-ln -s /var/www/html/files ~/files
+sudo ln -s /var/www/html/files ~/files
 
 # set up shadowsocks
-mkdir /etc/shadowsocks
-jq -n --arg password $sspassword (cat ./shadowsocks.config) > /etc/shadowsocks/shadowsocks.config
-mv ./shadowsocks.service /etc/systemd/system/ssserver.service
-chmod 664 /etc/systemd/system/ssserver.service
-systemctl enable /etc/shadowsocks/shadowsocks.service
+sudo mkdir /etc/shadowsocks
+sudo jq -n --arg password $sspassword (cat ./shadowsocks.config) > /etc/shadowsocks/shadowsocks.config
+sudo mv ./shadowsocks.service /etc/systemd/system/ssserver.service
+sudo chmod 664 /etc/systemd/system/ssserver.service
+sudo systemctl enable /etc/shadowsocks/shadowsocks.service
 
 # azcopy for weekly backup
 sudo echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod/ trusty main" > azure.list
 sudo cp ./azure.list /etc/apt/sources.list.d/
-apt-key adv --keyserver packages.microsoft.com --recv-keys B02C46DF417A0893
-mv azure.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys B02C46DF417A0893
+sudomv azure.list
 sudo apt-get update
 sudo apt-get install azcopy
 
 # set timezone as Asia/Shanghai
-timedatectl set-timezone Asia/Shanghai
+sudo timedatectl set-timezone Asia/Shanghai
 
 
 
